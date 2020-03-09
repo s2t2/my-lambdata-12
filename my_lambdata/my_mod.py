f082bfe5d46056423d8f4dd6be356eb305641b44
Mike Rossetti <s2t2@users.noreply.github.com>
2020-03-09 14:07:25 -0400

    Demonstrate the need to remove code from global scope of modules we want to be able to import

--- my_lambdata/my_mod.py
+++ my_lambdata/my_mod.py
@@ -8,3 +8,17 @@ def enlarge(n):
     Function will enlarge the number
     """
     return n * 100
+
+
+# this code breaks our ability to import enlarge from other files
+#
+# print("HELLO")
+# y = int(input("Please choose a number"))
+# print(y, enlarge(y))
+
+if __name__ == "__main__":
+    # only run the code below IF this script is invoked from the command-line
+    # not if it is imported from another
+    print("HELLO")
+    y = int(input("Please choose a number"))
+    print(y, enlarge(y))
