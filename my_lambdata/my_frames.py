8be1f664bf2d9dbd3f4263068572362b46f542ef
Mike Rossetti <s2t2@users.noreply.github.com>
2020-03-11 13:00:57 -0400

    Implement state name conversion functionality

--- my_lambdata/my_frames.py
+++ my_lambdata/my_frames.py
@@ -4,11 +4,22 @@
 
 import pandas
 
-# State abbreviation -> Full Name and visa versa.
-# FL -> Florida, etc. (Handle Washington DC and territories like Puerto Rico etc.)
+def add_state_names(my_df):
+    """
+    Add a column of state names to a dataframe that already has the abbrevs
+    Param my_df pandas.DataFrame should have a column of "abbrev"
+    Return a copy of the DataFrame with a new column called "name"
+    """
+    names_map = {"CT": "Conn", "CO": "Colorado", "CA": "Cali"}
+    #breakpoint() # python 3.7 or later, otherwise use pdb module
+    new_df = my_df.copy()
+    new_df["name"] = my_df["abbrev"].map(names_map) # see: https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.Series.map.html
+    return new_df
 
-df = pandas.DataFrame({"state": ["CT", "CO", "CA", "TX"]})
-print(df.head())
+df = pandas.DataFrame({"abbrev": ["CT", "CO", "CA", "TX"]})
+mutated_df = add_state_names(df) # desired invocation
+print(mutated_df.head())
 
-df2 = pandas.DataFrame({"state": ["AZ", "DC", "CO", "MI", "WI"]})
-print(df2.head())
+df2 = pandas.DataFrame({"abbrev": ["AZ", "DC", "CO", "MI", "WI"]})
+mutated_df2 = add_state_names(df2) # desired invocation
+print(mutated_df2.head())
